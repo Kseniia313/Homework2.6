7e2f4ae8f2f5d3029b5cb3eaf291d52a0eff97cd
Kseniia313 <kshome@mail.ru>
2023-06-21 19:17:07 +0300

    ДЗ второй вариант

--- src/main/java/pro/sky/employee/EmployeeController.java
+++ src/main/java/pro/sky/employee/EmployeeController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
-public abstract class EmployeeController {
+public class EmployeeController {
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {

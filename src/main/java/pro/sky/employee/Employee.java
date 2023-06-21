79b3abda2296e70cfc4c858b3d3312ff1178cea6
Kseniia313 <kshome@mail.ru>
2023-06-21 12:21:31 +0300

    ДЗ первый вариант

--- src/main/java/pro/sky/employee/Employee.java
+++ src/main/java/pro/sky/employee/Employee.java
@@ -1,4 +1,42 @@
 package pro.sky.employee;
 
+import java.util.Objects;
+
 public class Employee {
+    private String firstName;
+    public String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object employeer) {
+        if (this == employeer) return true;
+        if (employeer == null || getClass() != employeer.getClass()) return false;
+        Employee employee = (Employee) employeer;
+        return Objects.equals(firstName, employee.firstName)&&Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "firstName:" + firstName +"lasttName:"+ lastName;
+    }
 }
+
+
+
---
+++ src/main/java/pro/sky/employee/EmployeeService.java
@@ -0,0 +1,14 @@
+package pro.sky.employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+   Employee addEmployee(String firstName, String lastName);
+
+    Employee removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+    List<Employee> getAllEmployees ();
+}
